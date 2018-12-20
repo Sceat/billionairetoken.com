bee258cd2f0e8647b6b8092aad48eed637df2bf2
Sceat <11330271+Sceat@users.noreply.github.com>
2018-12-21 01:38:36 +0200

    return to top xbl style

--- src/comps/nav/returnToTop.vue
+++ src/comps/nav/returnToTop.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     transition(name="top")
         .return-to-top(v-if="scrolled" @click="scrollToTop()")
-            fa(fas="angle-up" size="3x" )
+            fa(fas="angle-up" size="2x" )
 </template>
 
 <script>
@@ -12,50 +12,50 @@ import { above, below } from '@core/misc/scrollFire'
 @Component
 export default class Top extends Vue {
 
-    scrolled = false
+  scrolled = false
 
-    @above(265)
-    show() {
-        if (!this.scrolled) this.scrolled = true
-    }
+  @above(265)
+  show() {
+    if (!this.scrolled) this.scrolled = true
+  }
 
-    @below(265)
-    hide() {
-        if (this.scrolled) this.scrolled = false
-    }
+  @below(265)
+  hide() {
+    if (this.scrolled) this.scrolled = false
+  }
 
-    scrollToTop() {
-        window.scroll({
-            top: 0,
-            behavior: 'smooth'
-        })
-    }
+  scrollToTop() {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 }
 </script>
 
 
 <style lang="stylus" scoped>
 .return-to-top
-    position fixed
-    bottom 1em
-    left 1em
-    z-index 99
-    width 50px
-    height @width
-    background #212121
-    display flex
-    justify-content center
-    align-items center
-    border-radius 5px
-    opacity .7
-    cursor pointer
+  position fixed
+  bottom 1em
+  right 1em
+  z-index 99
+  width 50px
+  height @width
+  background rgba(black, .4)
+  display flex
+  justify-content center
+  align-items center
+  border-radius 100px
+  opacity .7
+  cursor pointer
 
-    &.top-enter-active, &.top-leave-active
-        transition opacity 300ms
+  &.top-enter-active, &.top-leave-active
+    transition opacity 300ms
 
-    &.top-enter, &.top-leave-to
-        opacity 0
+  &.top-enter, &.top-leave-to
+    opacity 0
 
-    i
-        color invert(@background)
+  i
+    color white
 </style>
