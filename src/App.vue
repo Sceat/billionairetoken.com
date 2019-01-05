d69b36ea2b37854c14e5a7a1bef768f6adf59c29
Sceat <11330271+Sceat@users.noreply.github.com>
2019-01-05 19:08:27 +0200

    meta ready

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,46 @@
 
 <script>
 import { Vue, Component } from 'vue-property-decorator'
+import config from '@root/hydre.toml'
+
 
 @Component({
   components: {
     utils: () => import('@cmp/utils/index.vue')
+  },
+
+  metas() {
+    const configLang = config[this.$i18n.locale]
+
+    const og = [
+      { property: 'og:description', content: configLang.description },
+      { property: 'og:image', content: config.ogImage }
+    ]
+
+    const tw = [
+      { property: 'twitter:description', content: configLang.description },
+      { property: 'twitter:image', content: config.ogImage }
+    ]
+
+    config.og.forEach(meta => og.push(meta))
+    config.tw.forEach(meta => tw.push(meta))
+
+    return {
+      title: configLang.title,
+      titleTemplate: eval(configLang.titleTemplate),
+      meta: [
+        { name: 'description', content: configLang.description },
+        { property: 'og:title', content: configLang.ogTitle, template: configLang.ogTemplate },
+        { name: 'msapplication-TileColor', content: config.tileColor },
+        { name: 'theme-color', content: config.themeColor },
+        ...og,
+        ...tw
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: config.touchIconSize, href: '<%= BASE_URL %>img/icons/apple-touch-icon.png' },
+        { rel: 'mask-icon', href: '<%= BASE_URL %>img/icons/safari-pinned-tab.svg', color: config.maskIconColor }
+      ]
+    }
   }
 })
 export default class App extends Vue { }
@@ -24,10 +60,21 @@ export default class App extends Vue { }
 
 *
   sc-reset()
-  sc-disableScollBar()
   font-family 'Roboto', sans-serif
   outline none
   scroll-behavior smooth
+
+  &::-webkit-scrollbar-track
+    box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
+    background-color #555
+
+  &::-webkit-scrollbar
+    width 12px
+    background-color #F5F5F5
+
+  &::-webkit-scrollbar-thumb
+    box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
+    background-color #252525
 
   a
     :active
